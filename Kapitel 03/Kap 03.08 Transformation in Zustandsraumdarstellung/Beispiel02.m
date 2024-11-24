0dc2788fef551ce50515dfbb89aa38c2950c5de2
DrMichaelGloeckler <148858240+DrMichaelGloeckler@users.noreply.github.com>
2024-11-24 22:10:09 +0100

    Add files via upload

---
+++ Kapitel 03/Kap 03.08 Transformation in Zustandsraumdarstellung/Beispiel02.m
@@ -0,0 +1,17 @@
+%==========================================================================
+% Beispiel 02: Transformation in Zustandsraumdarstellung
+% 
+% Die Übertragungsfunktion lautet:  
+%
+%               Ua(s)    2*s^3 + 3*s^2 + 2*s + 1
+%       G(s) =  ----  =  -----------------------
+%               Ue(s)    7*s^3 + 2*s^2 + 4*s + 6
+%==========================================================================
+clear;          % Workspace aufräumen
+clc;            % Command Window löschen
+
+num=[2 3 2 1];  % Zählerpolynom als 1x3 Vektor
+                % (b3=0 muss nicht eingegeben werden)
+den=[7 2 4 6];  % Nennerpolynom als 1x4 Vektor
+
+[A,B,C,D] = tf2ss(num,den)
